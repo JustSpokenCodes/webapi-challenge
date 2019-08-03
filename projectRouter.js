2dc210808d43a42f6e8330036a1c4627897f3073
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-08-03 12:10:45 -0700

    finished projectRouter

--- projectRouter.js
+++ projectRouter.js
@@ -14,3 +14,61 @@ router.get('/', (req, res) => {
     })
 })
 
+router.get("/:id", (req, res) =>{
+    const id = req.params.id 
+    dbproject.getProjectActions(id)
+    .then( projectactions => {
+        res.status(200).json(projectactions)
+    })
+    .catch( error => {
+        res.status(500).json({error: "You cant get this data"})
+    })
+})
+
+router.post('/', (req, res) => {
+    const newProject = req.body
+
+    dbaction.insert(newProject)
+    .then(action => {
+        res.status(200).json(action)
+    })
+    .catch(error => {
+        res.status(500).json({error: "What are you trying to post?! Not the right thing, thats for sure"})
+    })
+})
+
+router.put('/:id', (req,res) => {
+    const updateProject = req.body
+    const id = req.params.id
+
+    dbaction.update(id, updateProject)
+    .then(project => {
+        res.status(200).json(project)
+    })
+    .catch(err => {
+        res.status(500).json({error: err, message: "You are not updating anything...yet"})
+    })
+
+})
+
+router.delete('/:id', (req, res) => {
+    const projectid = req.params.id
+
+    dbaction.remove(projectid)
+    .then(project => {
+        if(project) {
+            dbaction.remove(project).then(
+                removeproject => {
+                    res.status(201).json(removeproject)
+                }
+            )
+        }else {
+            res.status(404).json({error: err, message: "That user doesnt exist"})
+        }
+    })
+    .catch(error => {
+        res.status(500).json({message: "That user hasnt been removed"})
+    })
+})
+
+module.exports = router

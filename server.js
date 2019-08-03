4a657e312f560c3e9bdc1d3612d336d5da1407a5
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-08-03 11:29:11 -0700

    the server still isnt coming up, yay

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const server = express();
 const actionRouter = require('./actionRouter');
-const packageRouter = require('./packageRouter')
+const projectRouter = require('./projectRouter')
 
 server.use(express.json());
 server.use('/api/actions', actionRouter)
-server.use('api/package', packageRouter)
+server.use('api/package', projectRouter)
 
 server.get('/', (req, res) => {
     res.send(`<h2> What We Getting Into?!<h2>`)
